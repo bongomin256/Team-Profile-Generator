33323a23b0dd008f2a96314156718fe878f5c692
Godfrey Bongomin <bongomin@uw.edu>
2022-04-30 23:45:03 -0700

    Edited the sample.css file path

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -58,7 +58,7 @@ function generateHTML(myTeam) {
         <title>Team Profile Generator</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-        <link rel="stylesheet" href="/dist/sample.css">
+        <link rel="stylesheet" href="./sample.css">
     </head>
     <body>
         <header class="jumbotron pt-4 pb-4 text-center bg-danger text-white">
